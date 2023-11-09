67f883d416f2b46e601697dfc433f7d873c193c2
Prakash Dumpa <prakashdumpa143@gmail.com>
2023-11-09 09:27:25 +0530

    add refresh button

--- src/App.js
+++ src/App.js
@@ -18,41 +18,29 @@ import ErrorBoundary from "./components/ErrorBoundary";
 
 const App = () => {
   // const [co, setCo] = useState(0);
+
   return (
     // <context.Provider value={{ co, setCo }}>
-
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ErrorBoundary>
-              <Layout />
-            </ErrorBoundary>
-          }
-        >
-          <Route index element={<Home />} />
-          <Route
-            path="counter"
-            element={
-              <ErrorBoundary>
-                <Counter />
-              </ErrorBoundary>
-            }
-          />
-          <Route path="todo" element={<Todo />} />
-          {/* <Route path="/:id" element={<Ids />} /> */}
-          <Route path="/id" element={<Ids />} />
-          <Route path="/redux" element={<ReduxToolKit />} />
-          <Route path="/apicall" element={<ApiCallUsingRedux />} />
-          {/* Nested Routing */}
-          {/* <Route path="todo" element={<Todo />}>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="counter" element={<Counter />} />
+            <Route path="todo" element={<Todo />} />
+            {/* <Route path="/:id" element={<Ids />} /> */}
+            <Route path="/id" element={<Ids />} />
+            <Route path="/redux" element={<ReduxToolKit />} />
+            <Route path="/apicall" element={<ApiCallUsingRedux />} />
+            {/* Nested Routing */}
+            {/* <Route path="todo" element={<Todo />}>
             <Route path="nested1" element={<Nested1 />} />
             <Route path="nested2" element={<Nested2 />} />
           </Route> */}
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ErrorBoundary>
     </BrowserRouter>
     // </context.Provider>
   );
--- src/components/ErrorBoundary/index.js
+++ src/components/ErrorBoundary/index.js
@@ -1,26 +1,36 @@
 import { Component } from "react";
+import { Link } from "react-router-dom";
+
 class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError() {
-    console.log("-------------------------------------------");
-    return { hasError: true };
-  }
+  // static getDerivedStateFromError() {
+  //   console.log("-------------------------------------------");
+  //   return { hasError: true };
+  // }
 
   componentDidCatch(error, errorInfo) {
-    // this.setState({ hasError: true });
+    this.setState({ hasError: true });
     console.log(",,,,,,,,,,,,,,,,,,", error, errorInfo);
   }
+
   render() {
     console.log("Radhe RAdhe");
     if (this.state.hasError) {
       return (
-        <h1 className="text-3xl flex justify-center items-center flex-grow">
-          Something Went Wrong
-        </h1>
+        <div className="text-3xl flex flex-col h-screen justify-center items-center ">
+          <h1>Something Went Wrong</h1>
+          <button
+            onClick={() => window.location.reload()}
+            className="bg-red-700 px-4 py-2 rounded"
+            type="button"
+          >
+            <Link to="/">Refresh</Link>
+          </button>
+        </div>
       );
     }
     return this.props.children;
